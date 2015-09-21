2ef4bf0c355d43fc452b74dda91dc7ab7fdf8f8b
Sanjay <svamja@gmail.com>
2015-09-21 12:50:26 +0530

    "configurized" db entities, new view

--- app/Console/Commands/AppSetup.php
+++ app/Console/Commands/AppSetup.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Doctrine\CouchDB\CouchDBClient;
 use Exception;
+use Config;
 
 class AppSetup extends Command
 {
@@ -21,90 +22,6 @@ class AppSetup extends Command
      * @var string
      */
     protected $description = 'Create Database Structure';
-
-    protected $categories = [
-        "Basic Needs",
-        "Health",
-        "Social",
-        "Children & Youth",
-        "Women",
-        "Seniors & Specially Abled",
-        "Animals",
-        "Safety",
-        "Environment",
-        "Spiritual",
-        "Happiness",
-        "Others",
-    ];
-
-
-
-    protected $design_docs = [
-        [
-            "db" => "careapp_passions_db",
-            "_id" => "_design/categories",
-            "language" => "javascript",
-            "views" => [
-                "by_order" => [
-                    "map" => "function(doc) { if(doc.type == 'category') { emit([doc.order], doc); }  }"
-                ]
-            ]
-        ],
-        [
-            "db" => "careapp_passions_db",
-            "_id" => "_design/sub_categories",
-            "language" => "javascript",
-            "views" => [
-                "by_category" => [
-                    "map" => "function(doc) { if(doc.type == 'sub_category') { emit([doc.category_id], doc); }  }"
-                ]
-            ]
-        ],
-        [
-            "db" => "careapp_passions_db",
-            "_id" => "_design/passions",
-            "language" => "javascript",
-            "views" => [
-                "by_passion" => [
-                    "map" => "function(doc) { if(doc.type == 'passion') { emit([doc._id], doc); }  }"
-                ]
-            ]
-        ],
-        [
-            "db" => "careapp_passions_db",
-            "_id" => "_design/cities",
-            "language" => "javascript",
-            "views" => [
-                "all" => [
-                    "map" => "function(doc) { if(doc.type == 'city') { emit([doc._id], doc); }  }"
-                ]
-            ]
-        ],
-        [
-            "db" => "careapp_profiles_db",
-            "_id" => "_design/profiles",
-            "language" => "javascript",
-            "views" => [
-                "by_passion" => [
-                    "map" => "function(doc) { if(doc.type != 'profile' || !doc.passions) return; for(var i = 0, len = doc.passions.length; i < len; i++) emit(doc.passions[i].id, doc); }"
-                ]
-            ],
-            "filters" => [
-                "by_interest" => "function(doc, req) { return doc.city === req.query.city; }"
-            ]
-        ],
-        [
-            "db" => "careapp_messages_db",
-            "_id" => "_design/messages",
-            "language" => "javascript",
-            "views" => [
-                "by_passion_ts" => [
-                    "map" => "function(doc) { if(doc.passion_id && doc.posted_on) { emit([doc.passion_id, doc.posted_on], doc); } }"
-                ]
-            ]
-        ],
-        
-    ];
 
     /**
      * Create a new command instance.
@@ -135,6 +52,79 @@ class AppSetup extends Command
         return $text;
     }
 
+    protected function _upsert($couch, $doc) {
+        $doc_req = [];
+        if(empty($doc['_id'])) {
+            return false;
+        }
+        try {
+            $doc_req = $couch->findDocument($doc['_id']);
+            if($doc_req->status == 200 && $doc_req->body['_rev']) {
+                $couch->putDocument($doc, $doc['_id'], $doc_req->body['_rev']);
+            }
+            else {
+                $couch->putDocument($doc, $doc['_id']);
+            }
+        }
+        catch(Exception $e) {
+            throw($e);
+            return false;
+        }
+        return true;
+    }
+
+    protected function _create_users_dbs() {
+        
+        // Create Admin User
+        $couch = CouchDBClient::create(array(
+            'dbname' => '_users',
+        ));
+        $http_client = $couch->getHttpClient();
+        $http_client->request('PUT', '/_config/admins/' . env('COUCH_ADMIN_USER'), '"' . env('COUCH_ADMIN_PASS') . '"');
+        $this->info("Admin user (re)created.");
+
+
+        // Create Databases
+        $couch = CouchDBClient::create(array(
+            'dbname' => '_users',
+            'user' => env('COUCH_ADMIN_USER'),
+            'password' => env('COUCH_ADMIN_PASS'),
+        ));
+
+        $databases = Config::get("careapp.databases");
+        if(empty($databases)) {
+            $this->info("Warning: No Databases found in Config file.");
+            return;
+        }
+        foreach($databases as $database) {
+            try {
+                $couch->createDatabase($database);
+                $this->info("$database created");
+            }
+            catch(Exception $e) {
+                 $this->info("$database not created.");
+            }
+        }
+
+        // Create App User
+        $username = env("COUCH_APP_USER");
+        $user_id = "org.couchdb.user:$username";
+        $user = [
+            '_id' => $user_id,
+            'type' => 'user',
+            'roles' => [],
+            'name' => $username,
+            'password' => env("COUCH_APP_PASS"),
+        ];
+        try {
+            $couch->putDocument($user, $user['_id']);
+            $this->info("App user created");
+        }
+        catch(Exception $e) {
+            $this->info("App user not created.");
+        }
+
+    }
 
     protected function _create_categories() {
 
@@ -144,7 +134,13 @@ class AppSetup extends Command
             'password' => env('COUCH_APP_PASS'),
         ));
 
-        foreach($this->categories as $i => $category_text) {
+        $categories = Config::get("careapp.categories");
+        if(empty($categories)) {
+            $this->info("Warning: No Categories found in Config file.");
+            return;
+        }
+
+        foreach($categories as $i => $category_text) {
             $order = $i + 1;
             $category_slug = $this->_slug($category_text);
             $category_id = "cat-" . $category_slug;
@@ -169,19 +165,23 @@ class AppSetup extends Command
     }
 
     protected function _create_design_docs() {
-        foreach($this->design_docs as $design_doc) {
+        $design_docs = Config::get("careapp.design_docs");
+        if(empty($design_docs)) {
+            $this->info("Warning: No Design Docs definitions found in Config file.");
+            return;
+        }
+        foreach($design_docs as $design_doc) {
             $couch = CouchDBClient::create(array(
                 'dbname' => $design_doc['db'],
                 'user' => env('COUCH_ADMIN_USER'),
                 'password' => env('COUCH_ADMIN_PASS'),
             ));
             unset($design_doc['db']);
-            try {
-                $couch->putDocument($design_doc, $design_doc['_id']);
-                $this->info("Design Doc Created: " . $design_doc['_id']);
+            if($this->_upsert($couch, $design_doc)) {
+                $this->info("Design Doc Upserted: " . $design_doc['_id']);
             }
-            catch(Exception $e) {
-                $this->info("Unable to Create Design Doc: " . $design_doc['_id']);
+            else {
+                $this->info("Unable to Upsert Design Doc: " . $design_doc['_id']);
             }
         }
     }
@@ -194,61 +194,16 @@ class AppSetup extends Command
     public function handle()
     {
 
-            // Create Admin User
-            $couch = CouchDBClient::create(array(
-                'dbname' => '_users',
-            ));
-            $http_client = $couch->getHttpClient();
-            $http_client->request('PUT', '/_config/admins/' . env('COUCH_ADMIN_USER'), '"' . env('COUCH_ADMIN_PASS') . '"');
+            // Users and Databases
+            $this->info("-- USERS & DATABASES --");
+            $this->_create_users_dbs();
 
-            // Create Databases
-            $couch = CouchDBClient::create(array(
-                'dbname' => '_users',
-                'user' => env('COUCH_ADMIN_USER'),
-                'password' => env('COUCH_ADMIN_PASS'),
-            ));
-            $databases = [
-                "careapp_profiles_db",
-                "careapp_passions_db",
-                "careapp_messages_db",
-                "careapp_log_db", 
-            ];
-            foreach($databases as $database) {
-                try {
-                    $couch->createDatabase($database);
-                    $this->info("$database created");
-                }
-                catch(Exception $e) {
-                     $this->info("$database not created : " . $e->getMessage());
-                }
-            }
-
-            // Create App User
-            $username = env("COUCH_APP_USER");
-            $user_id = "org.couchdb.user:$username";
-            $user = [
-                '_id' => $user_id,
-                'type' => 'user',
-                'roles' => [],
-                'name' => $username,
-                'password' => env("COUCH_APP_PASS"),
-            ];
-            try {
-                $couch->putDocument($user, $user['_id']);
-                $this->info("App user created");
-            }
-            catch(Exception $e) {
-                $this->info("App user not created.");
-            }
-
-            // Create Categories
+            // Categories
             $this->info("-- CATEGORIES --");
             $this->_create_categories();
 
-            // Create Design Docs
+            // Design Docs
             $this->info("-- DESIGN DOCS --");
             $this->_create_design_docs();
-
-
      }
 }
---
+++ config/careapp.php
@@ -0,0 +1,97 @@
+<?php
+
+return [
+
+    "databases" => [
+        "careapp_profiles_db",
+        "careapp_passions_db",
+        "careapp_messages_db",
+        "careapp_log_db", 
+    ],
+
+    "categories" => [
+        "Basic Needs",
+        "Health",
+        "Social",
+        "Children & Youth",
+        "Women",
+        "Seniors & Specially Abled",
+        "Animals",
+        "Safety",
+        "Environment",
+        "Spiritual",
+        "Happiness",
+        "Others",
+    ],
+
+    "design_docs" => [
+        [
+            "db" => "careapp_passions_db",
+            "_id" => "_design/categories",
+            "language" => "javascript",
+            "views" => [
+                "by_order" => [
+                    "map" => "function(doc) { if(doc.type == 'category') { emit([doc.order], doc); }  }"
+                ]
+            ]
+        ],
+        [
+            "db" => "careapp_passions_db",
+            "_id" => "_design/sub_categories",
+            "language" => "javascript",
+            "views" => [
+                "by_category" => [
+                    "map" => "function(doc) { if(doc.type == 'sub_category') { emit([doc.category_id], doc); }  }"
+                ]
+            ]
+        ],
+        [
+            "db" => "careapp_passions_db",
+            "_id" => "_design/passions",
+            "language" => "javascript",
+            "views" => [
+                "by_passion" => [
+                    "map" => "function(doc) { if(doc.type == 'passion') { emit([doc._id], doc); }  }"
+                ]
+            ]
+        ],
+        [
+            "db" => "careapp_passions_db",
+            "_id" => "_design/cities",
+            "language" => "javascript",
+            "views" => [
+                "all" => [
+                    "map" => "function(doc) { if(doc.type == 'city') { emit([doc._id], doc); }  }"
+                ]
+            ]
+        ],
+        [
+            "db" => "careapp_profiles_db",
+            "_id" => "_design/profiles",
+            "language" => "javascript",
+            "views" => [
+                "by_passion" => [
+                    "map" => "function(doc) { if(doc.type != 'profile' || !doc.passions) return; for(var i = 0, len = doc.passions.length; i < len; i++) emit(doc.passions[i].id, doc); }"
+                ],
+                "by_city_passion" => [
+                    "map" => "function(doc) { if(doc.type != 'profile' || !doc.passions || !doc.city) return; for(var i = 0, len = doc.passions.length; i < len; i++) emit([doc.city, doc.passions[i].id], doc); }"
+                ]
+            ],
+            "filters" => [
+                "by_interest" => "function(doc, req) { return doc.city === req.query.city; }"
+            ]
+        ],
+        [
+            "db" => "careapp_messages_db",
+            "_id" => "_design/messages",
+            "language" => "javascript",
+            "views" => [
+                "by_passion_ts" => [
+                    "map" => "function(doc) { if(doc.passion_id && doc.posted_on) { emit([doc.passion_id, doc.posted_on], doc); } }"
+                ]
+            ]
+        ],
+        
+    ],
+
+];
